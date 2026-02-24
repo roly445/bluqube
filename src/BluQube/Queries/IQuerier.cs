7c49473cf98728ca0d4e8604ff761b15d29b8a83
Andrew Davis <andrew@deviouscreation.com>
2026-02-24 11:05:48 +0000

    fix: Remove old CommandRunner, ICommandRunner, QueryRunner, and IQueryRunner duplicate files

--- src/BluQube/Queries/IQuerier.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace BluQube.Queries;
-
-public interface IQueryRunner
-{
-    Task<QueryResult<TQueryResult>> Send<TQueryResult>(
-        IQuery<TQueryResult> request, CancellationToken cancellationToken = default)
-        where TQueryResult : IQueryResult;
-}
